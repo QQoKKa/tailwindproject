aa95980408fc3edfa3e1960d2f2711a39db3e5ba
praktyka <balcerzakmateusz2@gmail.com>
2023-03-17 10:25:31 +0100

    Basic Sidebar
    basic sidebar creation

---
+++ tailwind-project/src/sidebar.js
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-44
+                        flex flex-col
+                        bg-sidebarblue text-white ">
+            <i>głowna</i>
+            <i>finanse</i>
+            <i>pracownicy</i>
+            <i>zadania</i>
+        </div>
+    );
+};
+
+export default Sidebar;
--- tailwind-project/tailwind.config.js
+++ tailwind-project/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'sidebarblue': '#6E44FF',
+        'infored': '#E63946',
+        'underscoregreen': '#00CC66'
+      },
+    },
   },
   plugins: [],
 }
