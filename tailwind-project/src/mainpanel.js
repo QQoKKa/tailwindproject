ce20808abcc9909d9b1eb1cf8c9ab98f46c1d523
praktyka <balcerzakmateusz2@gmail.com>
2023-03-20 08:37:40 +0100

    Mainpanel1.0
    basic mainpanel

--- tailwind-project/src/mainpanel.js
+++ tailwind-project/src/mainpanel.js
@@ -1,9 +1,13 @@
 const MainPanel = () => {
     return (
         <div className="bg-gray-100 min-h-screen p-4  
-                        flex flex-auto justify-center "> 
-            <div className=" bg-red-500 h-[400px] w-[800px]">
-                <p>1</p>
+                        flex flex-auto justify-center items-center "> 
+            <div className=" h-[600px] w-[1100px] ml-32 grid grid-cols-2 grid-rows-2  gap-4">
+                <div className="gridpanel"> Zysk </div>
+                <div className="gridpanel"> Najlepszy pracownik </div>
+                <div className="gridpanel"> Aktualne zadania dla ciebie </div>
+                <div className="gridpanel"> Twoje wyniki </div>
+
             </div>
         </div>
     );
