c8b8fe201c8ed9588b975b06058ef69f10c81cec
praktyka <balcerzakmateusz2@gmail.com>
2023-03-20 11:42:06 +0100

    Mainpanel1.4
    basic  look of your activities

--- tailwind-project/src/mainpanel.js
+++ tailwind-project/src/mainpanel.js
@@ -1,4 +1,7 @@
 const MainPanel = () => {
+    const tasks_done = 10;
+    const tasks_working = 5;
+    const salary = 3520;
     let last_month = 1000;
     let this_month = 3000;
     const best_employee = "Jan Kowalski";
@@ -34,7 +37,7 @@ const MainPanel = () => {
                 </div>
                 <div className="gridpanel flex flex-col"> Aktualne zadanie dla ciebie <br></br>
       {tasks.map((item) => (
-       <p className=" border-b-2 border-underscoregreen "> <p>{item.name}</p> <p> {item.description}</p>  Deadline: {item.deadline} <p> priority: {item.priority}</p>  </p>
+       <p className=" "> <p>{item.name}</p> <p> {item.description}</p>  Deadline: {item.deadline} <p> priority: {item.priority}</p>  </p>
       ))}
                 <button type="button" class="focus:outline-none text-white bg-green-600 hover:bg-green-800 focus:ring-4
                     focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800
@@ -43,8 +46,19 @@ const MainPanel = () => {
                 </button>
 
                 </div>
-                <div className="gridpanel"> Twoje wyniki </div>
-
+                    <div className="gridpanel"> Twoje wyniki <br></br>
+                    <div className="flex flex-row justify-center space-x-5 ">
+                    <div className="text-center">Wykonane zadania
+                        <p>{tasks_done}</p>
+                    </div>
+                    <div className="text-center">Trwające zadania
+                        <p>{tasks_working}</p>
+                    </div>
+                    <div className="text-center">aktualna wypłata
+                        <p>{salary}zł</p>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
