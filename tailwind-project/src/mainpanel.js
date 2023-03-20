f917fa3cadadb3d803d46d7faa7d6b50caf7d05a
praktyka <balcerzakmateusz2@gmail.com>
2023-03-20 10:09:27 +0100

    Mainpanel1.1
    concept for finanse main

--- tailwind-project/src/mainpanel.js
+++ tailwind-project/src/mainpanel.js
@@ -1,9 +1,27 @@
 const MainPanel = () => {
+    let last_month = 1000;
+    let this_month = 3000;
+    function calculateProfit() {
+        return this_month - last_month;
+    }
+    function calculateProfitPercent() {
+        return (calculateProfit() / last_month) * 100; 
+    }
     return (
         <div className="bg-gray-100 min-h-screen p-4  
-                        flex flex-auto justify-center items-center "> 
+                        flex flex-auto justify-center items-center"> 
             <div className=" h-[600px] w-[1100px] ml-32 grid grid-cols-2 grid-rows-2  gap-4">
-                <div className="gridpanel"> Zysk </div>
+                <div className="gridpanel flex"> Zyski firmy
+                <div><br></br> ten miesiąc {this_month}zł</div>
+                <div><br></br> poprzedni miesiąc {last_month}zł</div>
+                <div><br></br> Zysk {calculateProfit()}zł</div>
+                    <div className="bg-red-500 w-[200px] h-[200px] flex justify-center items-center place-self-center ml-28 ">
+                        <div className="bg-blue-500 w-[150px] h-[150px]
+                                          rounded-full flex items-center justify-center
+                                        text-white font-bold text-3xl 
+                                           border-green-700 border-4">{calculateProfitPercent()}%</div>
+                    </div>
+                </div>
                 <div className="gridpanel"> Najlepszy pracownik </div>
                 <div className="gridpanel"> Aktualne zadania dla ciebie </div>
                 <div className="gridpanel"> Twoje wyniki </div>
