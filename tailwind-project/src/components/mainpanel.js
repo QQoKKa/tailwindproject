66a61ea660f22d46220d3bbef9645fe9b6aa3144
praktyka <balcerzakmateusz2@gmail.com>
2023-04-04 11:19:09 +0200

    Minor1.2
    some minor changes

--- tailwind-project/src/components/mainpanel.js
+++ tailwind-project/src/components/mainpanel.js
@@ -168,7 +168,7 @@ const handleOpenTaskList = () => {
         )} 
 
             <div className=" h-[600px] w-[1100px] ml-32 grid grid-cols-2 grid-rows-2  gap-4">
-                <div className=" text-center grind gridpanel"> <p> Witaj {empData && empData[0] ? empData[0].name : "brak danych"}</p> <br></br>
+                <div className=" text-2xl text-center grind gridpanel"> <p> Witaj {empData && empData[0] ? empData[0].name : "brak danych"}</p> <br></br>
                 {!showTaskList && (
                 <button onClick={handleOpenTaskList}  class=" mt-16 text-white bg-green-600 hover:bg-green-700 shadow-lg 
                     font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 
